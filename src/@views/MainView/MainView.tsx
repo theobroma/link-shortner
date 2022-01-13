92725bfdcf68f61d8ac0e12b1c828debe791a771
Alexandr Siryj <theobroma222@gmail.com>
2022-01-13 05:17:40 +0200

    basic RHF forms

--- src/@views/MainView/MainView.tsx
+++ src/@views/MainView/MainView.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+// import BasicExample from '../../@components/BasicExample';
+import JobForm from '../../@components/JobForm';
 
 const MainView: React.FC = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="py-20 px-10 bg-purple-700 min-h-screen">
+        {/* <BasicExample /> */}
+        <JobForm />
+      </div>
+    </div>
+  );
 };
 
 export default MainView;

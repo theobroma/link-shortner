7ffc2dde1d6565c180b4b33ba50507882cfc9a97
Alexandr Siryj <theobroma222@gmail.com>
2022-04-15 19:09:05 +0300

    upd libs and react v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import './wdyr'; // <--- first import
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './@store/configureStore';
 import { AppContainer } from './@routes/AppContainer';
@@ -15,7 +15,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<div>loading...</div>} persistor={persistor}>
@@ -23,7 +25,6 @@ ReactDOM.render(
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change

cb588e1ec22957ee3dd3c8c91cf46d87e6e7b9ce
Alexandr Siryj <theobroma222@gmail.com>
2022-09-09 20:58:56 +0300

    rm unused forms

--- src/@views/MainView/MainView.tsx
+++ src/@views/MainView/MainView.tsx
@@ -1,14 +1,10 @@
 import { AppForm } from '../../@components/AppForm';
-// import BasicExample from '../../@components/BasicExample';
-// import JobForm from '../../@components/JobForm';
 import { Shortens } from '../../@components/Shortens';
 
 const MainView = () => {
   return (
     <div>
       <div className="min-h-screen bg-purple-700 py-20 px-10">
-        {/* <BasicExample /> */}
-        {/* <JobForm /> */}
         <AppForm />
         <Shortens />
       </div>
